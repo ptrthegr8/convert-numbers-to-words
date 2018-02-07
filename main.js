da59a495fb806c1e7e5984f88f395de0d6647022
Peter Marsh <p.marsh@mac.com>
2018-02-07 14:41:45 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,84 @@
+
+const thousandName = ["", "one-thousand"];
+const hundredName = ["","one-hundred", "two-hundred", "three-hundred", "four-hundred", "five-hundred", "six-hundred", "seven-hundred", "eight-hundred", "nine-hundred"];
+const teenName = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+const tenName = ["", "ten", "twenty", "thirty", "forty", "fifty",
+         "sixty", "seventy", "eighty", "ninety"];
+const oneName = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+// 1-9
+for (var i=1; i<=9; i++) {
+document.write(oneName[i] + "<br />");
+} 
+//10
+document.write(tenName[1] + "<br />");
+// 11-19
+for (var i=1; i<=9; i++) {
+document.write(teenName[i] + "<br />");
+}
+//20
+document.write(tenName[2] + "<br />");
+//21-29
+for (var i=1; i<=9; i++) {
+document.write(tenName[2] + oneName[i] + "<br />");
+}
+//30
+document.write(tenName[3] + "<br />");
+//31-39
+for (let i=1; i<=9; i++) {
+document.write(tenName[3] + oneName[i] + "<br />");
+}
+//40
+document.write(tenName[4] + "<br />");
+//41-49
+for (let i=1; i<=9; i++) {
+document.write(tenName[4] + oneName[i] + "<br />");
+}
+//50
+document.write(tenName[5] + "<br />");
+//51-59
+for (let i=1; i<=9; i++) {
+document.write(tenName[5] + oneName[i] + "<br />");
+}
+//60
+document.write(tenName[6] + "<br />");
+//61-69
+for (let i=1; i<=9; i++) {
+document.write(tenName[6] + oneName[i] + "<br />");
+}
+//70
+document.write(tenName[7] + "<br />");
+//71-79
+for (let i=1; i<=9; i++) {
+document.write(tenName[7] + oneName[i] + "<br />");
+}
+//80
+document.write(tenName[8] + "<br />");
+//81-89
+for (let i=1; i<=9; i++) {
+document.write(tenName[8] + oneName[i] + "<br />");
+}
+//90
+document.write(tenName[9] + "<br />");
+
+//91-99 
+for (let i=1; i<=9; i++) {
+document.write(tenName[9] + oneName[i] + "<br />");
+} 
+
+// hundred
+document.write(hundredName[1] + "<br />");
+
+// //101-109
+// for (let i=1; i<=9; i++) {
+// document.write(hundredName[1] + oneName[i] + "<br />");
+// }
+// //110
+// //111-119
+// //120
+// //121-129
+// //
+
+// // thousand
+// document.write(thousandName[1]);
+
+
